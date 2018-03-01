f3f38f1692c357ce8743698e06cd94a00cbf978c
Nevethan Alagaratnam <nevethan94@gmail.com>
2018-03-01 17:10:18 +0100

    Code for Exercise 1,2 and litlle for exercise 3

--- PCA.R
+++ PCA.R
@@ -37,12 +37,29 @@ for(i in 1:length(idList)){
 dataset.all <- as.data.frame(id)
 
 #Shuffle data
-dataset.all <- datasetShuffle(dataset.all)
+
 
 train <- 1:(nrow(dataset.all)/2)
+train <- 1:116000 # 29 persons
 ###### Exercise 2.1 #####
-
 #Split data 50/50
+
+# #Disjunct - 57 members in total
+# dataset.train <- dataset.all[train,]
+# dataset.test <- dataset.all[-train,]
+# 
+# dataset.train <- datasetShuffle(dataset.train)
+# dataset.test <- datasetShuffle(dataset.test)
+# 
+# dataset.train <- dataset.train[,-1]
+# dataset.test <- dataset.test[,-1]
+# 
+# dataset.train.labels <- factor(dataset.train[,1])
+# dataset.test.labels <- factor(dataset.test[,1])
+
+#All persons
+dataset.all <- datasetShuffle(dataset.all)
+
 dataset.train <- dataset.all[train,-1]
 dataset.test <- dataset.all[-train,-1]
 
@@ -51,6 +68,7 @@ dataset.test.labels <- factor(dataset.all[-train,1])
 
 PCA.obj <- prcomp(dataset.train)
 
+##### Exercise 2.1.2 #####
 #plot results of PCA object
 require(graphics)
 
@@ -68,10 +86,129 @@ lines(PropVariancePCA[1:20], col= "red", lwd = 3)
 plot(main = "Cumulative Proportion of Variance", CumuPCA[1:20], xlab = "Principle Components", ylab = "Cumulative Proportion of Variance", col = "blue", pch = 4, cex = 2, lwd=3)
 lines(CumuPCA[1:20], col= "red", lwd = 3)
 
+##### Exercise 2.1.3 and Exercise 2.1.4 #####
+#PC's representing 80 % of the accumulated variance 
+#REMEMBER to vary k - Three values are enough
+PC.80 <- PCA.obj$x[, CumuPCA < 0.8] # Results in 14 PCs
 
-conMatrix(PCA.obj, dataset.test.labels) #My own implementation in Methods.R. It gives accuracy, preciseness etc. 
+time.start <- Sys.time()
+model <- knn(dataset.train[,1:14], dataset.test[,1:14], dataset.train.labels[1:14], 3)
+time.end <- Sys.time()
 
-model <- knn(PCA.obj, dataset.train.labels, dataset.test.labels, 5)
+#Run time
+print(time.end-time.start)
+
+#Performance
+accuracy <- acc(model, dataset.test.labels)
+
+#PC's representing 90 % of the accumulated variance
+PC.90 <- PCA.obj$x[, CumuPCA < 0.9] #Results in 23 PCs
+
+time.start <- Sys.time()
+model <- knn(dataset.train[,1:23], dataset.test[,1:23], dataset.train.labels[1:23], 3)
+time.end <- Sys.time()
+
+#Run time
+print(time.end-time.start)
+
+#Performance
+accuracy <- acc(model, dataset.test.labels)
+
+#PC's representing 95 % of the accumulated variance
+PC.95 <- PCA.obj$x[, CumuPCA < 0.95] # Results in 34 PCs
+
+time.start <- Sys.time()
+model <- knn(dataset.train[,1:34], dataset.test[,1:34], dataset.train.labels[1:34], 3)
+time.end <- Sys.time()
+
+#Run time
+print(time.end-time.start)
+
+#Performance
+accuracy <- acc(model, dataset.test.labels)
+
+#PC's representing 99 % of the accumulated variance
+PC.99 <- PCA.obj$x[, CumuPCA < 0.99] # Results in 72 PCs
+
+time.start <- Sys.time()
+model <- knn(dataset.train[,1:72], dataset.test[,1:72], dataset.train.labels[1:72], 3)
+time.end <- Sys.time()
+
+#Run time
+print(time.end-time.start)
+
+#Performance
+accuracy <- acc(model, dataset.test.labels)
+
+##### Exercise 2.2.1 #####
+normalize() #The best Dataset from Exercise 2.1.3 and Exercise 2.1.4
+
+folds <- createFolds(dataset$V1, 10) #Change the dataset
+
+a <- list()
+
+for(i in 1:length(folds)){
+  cross.test <- dataset[-folds[[i]],-1]
+  cross.train
+  
+  cross.test.labels 
+  cross.train.labels
+  
+  time.start <- Sys.time()
+  model <- knn(cross.train, cross.test, cross.train.labels,k) #Change k values
+  time.end <- Sys.time()
+  
+  a[i] <- acc(model, cross.test.labels) #accuracy
+  
+}
+
+#Plot accuracy for each fold
+plot(a)
+
+#Apply Normalization before PCA
+normalize(dataset.all)
+
+dataset.all <- datasetShuffle(dataset.all)
+
+dataset.train <- dataset.all[train,-1]
+dataset.test <- dataset.all[-train,-1]
+
+dataset.train.labels <- factor(dataset.all[train,1])
+dataset.test.labels <- factor(dataset.all[-train,1])
+
+PCA.obj <- prcomp(dataset.train)
+
+folds <- createFolds(dataset$V1, 10) #Change the dataset
+
+a <- list()
+
+for(i in 1:length(folds)){
+  cross.test <- dataset[-folds[[i]],-1]
+  cross.train
+  
+  cross.test.labels 
+  cross.train.labels
+  
+  time.start <- Sys.time()
+  model <- knn(cross.train, cross.test, cross.train.labels,k) #Change k values
+  time.end <- Sys.time()
+  
+  a[i] <- acc(model, cross.test.labels) #accuracy
+  
+}
+
+##### Exercise 2.3.1 #####
+id <- loadSinglePersonsData(DPI,group,member,folder)
+imageSize <- sqrt(ncol(id) - 1)
+imageM <- matrix( id[cipherNumber,2:ncol(id)],nrow =
+                    imageSize,ncol = imageSize,byrow = FALSE)
+
+imageM <- rotate(imageM) # rotate is a function to rotate the image
+
+image( imageM )
+
+
+
 
 
 
