6ba700536f19cd90a2abe25f7a8bc4a471f3ddd9
Nevethan Alagaratnam <nevethan94@gmail.com>
2018-03-16 13:40:27 +0100

    First Exercise is done

--- K-Means.R
+++ K-Means.R
@@ -1,6 +1,6 @@
 
 #Source loading of images and k-clustering
-source('C:/Users/Bruger/Desktop/k-clustering.R')
+source('C:/Users/Bruger/Desktop/SM-Exercises/k-clustering.R')
 
 id <- loadSinglePersonsData(100,4,0,folder)
 id <- data.frame(id)
@@ -101,9 +101,56 @@ for(i in length(folds)){
 }
 
 ##### Exercise 3.1.3 #####
-#Load personImages for 30 personer - 15/15
-
+## Prepre the data ##
+#Load personImages for 40 personer - 20/20
 #Repeat the same process from exercise 3.1.1
+#Disjunct dataset for 40 persons. 
+dataset.train <- datasetShuffle(dataset.all[1:80000,])
+dataset.test <- datasetShuffle(dataset.all[80001:160000,])
+
+dataset.train.labels <- factor(dataset.train[1:80000,1])
+dataset.test.labels <- factor(dataset.test[80001:160000,1])
+
+dataset.train <- normalize(dataset.train[1:80000,-1])
+dataset.test <- normalize(dataset.test[80001:160000,-1])
+
+set.seed(1234)
+#set.seed(2345)
+
+cipher_cluster <- c()
+label_cluster <- c()
+numberOfClusters <-  100
+
+#Performing K-means clusering of each cipher individually for the training set. 
+# -> represent the training data as a number of cluster centroids
+for( i in 0:9) {
+  clusterData <- kmeans(dataset.train[ dataset.train.labels == i, ],numberOfClusters)
+  cipher_cluster[[i + 1]] <- clusterData$centers
+  label_cluster[[i + 1]] <- c(1:numberOfClusters)*0 + i
+}
+
+train_labels <- factor(unlist(label_cluster))
+train_data <- cipher_cluster[[1]]
+for( i in 2:10) {
+  train_data <- rbind(train_data,cipher_cluster[[i]])
+}
+
+#Performing knn on the training data (cluster centroids)
+# K values 3,5 og 7
+# Cluster 100, 50 og 25
+time.start <- Sys.time()
+model <- knn(train_data, dataset.test,train_labels,3)
+time.end <- Sys.time()
+
+print(time.end-time.start)
+
+result <- confusionMatrix(dataset.test.labels, model)
+sum(diag(result$table))/sum(result$table)
+
+
+
+
+
 
 
 
