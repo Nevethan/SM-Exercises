4793947b88420c94dfb1d7c74b8a6764a5153611
Nevethan Alagaratnam <nevethan94@gmail.com>
2018-04-26 12:15:55 +0200

    5.1 mostly done

---
+++ SVM and NN.R
@@ -0,0 +1,163 @@
+library(RSNNS)
+library(devtools)
+
+##### Preproccessing #####
+source('C:/Users/Bruger/Desktop/Statistical Mashine Learning/BaseFolder/loadImage.R')
+
+#Get all data from folder
+getAllData <- function(dataList){
+  id <- data.frame()
+  idList <- list()
+  for(i in 1:length(dataList))
+  {
+    if( length(dataList[[i]]) > 0  ){
+      for(j in 1:length(dataList[[i]])){
+        idTemp <- loadSinglePersonsData(100,i - 1, dataList[[i]][j] ,folder)
+        idList <- append(idList, list(idTemp))
+      }
+    }
+  }
+  return(idList)
+}
+
+folder<- "C:/Users/Bruger/Desktop/Statistical Mashine Learning/preProcessed/2018/group"
+
+datalist <- list(  list( 1) ,list( 1, 2 ), list( 1, 2, 3 ),   list( 1, 2, 3 ), list( 1, 0, 4, 2, 3 ), 
+                   list( 1, 5, 4, 2, 3 ), list( 0, 2, 3 ), list( 1 ), list( 1, 2, 3 ), list( 1, 2, 3 ), 
+                   list( 1, 2, 3 ), list( 1, 4, 2, 3 ), list( 1, 2, 3 ), list( 1, 2 ), list( 1, 2, 3 ), 
+                   list( 1, 2 ), list( 1, 4, 2, 3 ), list( 1, 4, 2, 3 ), list( 1, 2, 3 ))
+
+idList <- getAllData(datalist)
+
+for(i in 1:length(idList)){
+  idTemp <- idList[i]
+  idTemp <- data.frame(idTemp)
+  id <- as.data.frame(rbind(id,idTemp))
+}
+
+dataset.all <- as.data.frame(id)
+
+#Split data 50/50
+
+#Disjunct - 20 members in total
+dataset.train <- dataset.all[1:40000,]
+dataset.test <- dataset.all[40001:80000,]
+
+dataset.train <- datasetShuffle(dataset.train)
+dataset.test <- datasetShuffle(dataset.test)
+
+dataset.train.labels <- factor(dataset.train[,1])
+dataset.test.labels <- factor(dataset.test[,1])
+
+dataset.train <- dataset.train[,-1]
+dataset.test <- dataset.test[,-1]
+
+##### Exericse 5.1.1 #####
+#Format the training classes so it matches a neural net with N inputs and 10
+#outputs where each of the outputs matches a given class
+
+lev <- levels(dataset.train$X1)
+
+nnTrainingClass <- matrix(nrow = length(dataset.train$X1), ncol = 10, data = 0)
+
+for(i in 1:length(dataset.train$X1)) {
+  matchList <- match(lev,toString(dataset.train$X1[i]))
+  matchList[is.na(matchList)] <- 0
+  nnTrainingClass[i,] <- matchList
+}
+
+trainingClass <- as.data.frame(nnTrainingClass) #our targets (labels)
+
+##### Exercise 5.1.2 #####
+#Train a neural network with N inputs and 10 outputs
+
+#all 'na' values are replaced with '0'
+dataset.train <- dataset.train[is.na(dataset.train)] <- 0
+
+#Training the neural network with standard backpropagation 
+nn.model <- mlp(dataset.train, trainingClass, learnFunc = "Std_Backpropagation", c(5))
+#Learning Algorithms
+nn.model <- mlp(dataset.train,trainingClass, size = c(3), maxit = 1000, learnFunc = "Std_Backpropagation")
+
+##### Exercise 5.1.3 #####
+#Evaluation of the Neural network
+
+#Code from learning algorithms
+#Predictions 
+predictions <- predict(model.mlp, t(t(x)))
+#Error over iterations (Epochs)
+plotIterativeError(model.mlp)
+
+#Mean Squared Error
+mse <- mean(( x - predictions)^2)
+
+#Root Mean Squared Error 
+rmse <- function(error) # error <- actual - predicted
+{
+  sqrt(mean(error^2))
+}
+
+rmse(x-predictions)
+
+#Inspiration code from exercise 
+#Remember to run 'predictions'
+responselist <- matrix(nrow = length(predictions[,1]), ncol = 1, data = "Na")
+
+for(i in 1:nrow(predictions)) {
+  responselist[i,] <- toString( which(predictions[i,]==max(predictions[i,])) - 1 )
+}
+
+responselist <- data.frame(responselist)
+responselist[,1] <- as.factor(responselist[,1])
+
+# Calculating the accuracy
+agreement_rbf <- responselist[,1] == test[,1]
+table(agreement_rbf)
+prop.table(table(agreement_rbf))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##### Exericse 5.1.4 #####
+#Try different parameters - number of neurons, number of hidden layers and differet learning parameters
+
+neurons <- c(5,10,15,20)
+
+for(i in length(neurons)){
+  nn.model <- mlp(dataset.train,trainingClass, size = c(neurons[i]), maxit = 1000, learnFunc = "Std_Backpropagation")
+  
+  predictions <- predict(nn.model, dataset.test)
+  
+  responselist <- matrix(nrow = length(predictions[,1]), ncol = 1, data = "Na")
+  
+  for(i in 1:nrow(predictions)) {
+    responselist[i,] <- toString( which(predictions[i,]==max(predictions[i,])) - 1 )
+  }
+  responselist <- data.frame(responselist)
+  responselist[,1] <- as.factor(responselist[,1])
+  
+  # Calculating the accuracy
+  agreement_rbf <- responselist[,1] == test[,1]
+  table(agreement_rbf)
+  prop.table(table(agreement_rbf))
+}
+
+
+
+
