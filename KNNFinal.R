8251529c008db2a3d888d20548bb155f9ae90156
Spider <Spiderixius@users.noreply.github.com>
2018-05-25 11:13:13 +0200

    Added timestamp.

--- KNNFinal.R
+++ KNNFinal.R
@@ -3,7 +3,20 @@ source("dataLoading.R")
 
 folder <- "../../SML_Data/preProcessed/2018/group"
 
-result <- getAllPersonsInData(folder, 10, 70)
+result <- getAllPersonsInData(folder, 2, 70)
 
+set.seed(1234)
+
+time.start <- Sys.time() 
 model <- knn(result$train, result$test, result$train.labels, 3)
+time.end <- Sys.time()
+
+print(time.end-time.start)
+
 acc(model, result$test.labels)
+
+
+
+
+
+
